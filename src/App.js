6db581d79628b77f1e57f9f05b83112a3252cd11
Fatima0221 <97350892+Musaddique221@users.noreply.github.com>
2022-08-26 15:30:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,165 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from "react";
+import "./App.css";
+import react from "react";
 
 function App() {
+  const [date, setDate] = useState("");
+  const [showMessage, setshowMessage] = useState("");
+  function reverseStr(str) {
+    var listOfChars = str.split("");
+    var reverseListOfChars = listOfChars.reverse();
+    var reversedStr = reverseListOfChars.join("");
+    return reversedStr;
+  }
+
+  function isPalindrome(str) {
+    var reverse = reverseStr(str);
+    return str === reverse;
+  }
+
+  function convertDateToStr(date) {
+    var dateStr = { day: "", month: "", year: "" };
+
+    if (date.day < 10) {
+      dateStr.day = "0" + date.day;
+    } else {
+      dateStr.day = date.day.toString();
+    }
+
+    if (date.month < 10) {
+      dateStr.month = "0" + date.month;
+    } else {
+      dateStr.month = date.month.toString();
+    }
+
+    dateStr.year = date.year.toString();
+    return dateStr;
+  }
+
+  function getAllDateFormats(date) {
+    var dateStr = convertDateToStr(date);
+
+    var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+    var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+    var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+  }
+
+  function checkPalindromeForAllDateFormates(date) {
+    var listOfPalindromes = getAllDateFormats(date);
+
+    var flag = false;
+
+    for (var i = 0; i < listOfPalindromes.length; i++) {
+      if (isPalindrome(listOfPalindromes[i])) {
+        flag = true;
+        break;
+      }
+    }
+    return flag;
+  }
+  function isLeapYear(year) {
+    if (year % 400 === 0) {
+      return true;
+    }
+    if (year % 100 === 0) {
+      return false;
+    }
+    if (year % 4 === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  function getNextDate(date) {
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (month === 2) {
+      if (isLeapYear(year)) {
+        if (day > 29) {
+          day = 1;
+          month++;
+        }
+      } else {
+        if (day > 28) {
+          day = 1;
+          month++;
+        }
+      }
+    } else {
+      if (day > daysInMonth[month - 1]) {
+        day = 1;
+        month++;
+      }
+    }
+    if (month > 12) {
+      month = 1;
+      year++;
+    }
+    return {
+      day: day,
+      month: month,
+      year: year
+    };
+  }
+
+  function getNextPalindromeDate(date) {
+    var counter = 0;
+    var nextDate = getNextDate(date);
+
+    while (1) {
+      counter++;
+      var isPalindrome = checkPalindromeForAllDateFormates(nextDate);
+      if (isPalindrome) {
+        break;
+      }
+      nextDate = getNextDate(nextDate);
+    }
+    return [counter, nextDate];
+  }
+
+  const inputHandler = (e) => {
+    var bdayStr = e.target.value;
+    if (bdayStr !== "") {
+      var listOfdate = bdayStr.split("-");
+    }
+
+    var inputDate = {
+      day: Number(listOfdate[2]),
+      month: Number(listOfdate[1]),
+      year: Number(listOfdate[0])
+    };
+
+    setDate(inputDate);
+  };
+
+  function clickHandler(e) {
+    var showMessage;
+
+    var isPalindrome = checkPalindromeForAllDateFormates(date);
+    if (isPalindrome) {
+      showMessage = "yay! your birthday is palindrome🥳🥳🥳";
+    } else {
+      var [counter, nextDate] = getNextPalindromeDate(date);
+      showMessage = `The next palindrome date is ${nextDate.day}-${nextDate.month}-${nextDate.year} and you missed it by ${counter} days😔`;
+    }
+    setshowMessage(showMessage);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Palindrome Birthday🎂</h1>
+      <input onChange={inputHandler} type="date" />
+      <button onClick={clickHandler}>show</button>
+      <p> {showMessage}</p>
     </div>
   );
 }
